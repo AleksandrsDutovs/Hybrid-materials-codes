25d441375c888239eb0b9fce4e4af70291075dbc
AleksandrsDutovs <67013442+AleksandrsDutovs@users.noreply.github.com>
2021-06-01 18:25:47 +0300

    Update bruggeman.m

--- Multilayer - R/bruggeman.m
+++ Multilayer - R/bruggeman.m
@@ -1,4 +1,4 @@
-function nt = bruggeman(fa,ea,eb)
+function nt = bruggeman(fa,ea,eb)   #Simulation of PAAO barrier layer complex refractive index by Bruggeman approximation 
 
 e1 = - ((sqrt((9*eb.^2-18*ea.*eb+9*ea.^2)*fa.^2 + ((-12*eb.^2)+18*ea.*eb-6*ea.^2)*fa+4*eb.^2+4*ea.*eb+ea.^2))
 +((3*eb-3*ea)*fa-2*eb+ea))/4 ;
